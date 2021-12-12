2defa0ccf70d9fa67a50991c5b3f9db7ed183c58
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-12-12 14:09:35 +0000

    added react and nwjs to the project

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. This is the boilerplate code, ready for the track client.
+          Edit <code>src/App.js</code> and save to reload. This is the boilerplate code, ready for the trakt client.
         </p>
         <a
           className="App-link"
