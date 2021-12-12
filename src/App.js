2dbd9503279397aab63a620b24779192242da182
crxssed7 <48864420+crxssed7@users.noreply.github.com>
2021-12-12 14:08:49 +0000

    setup the project to use react + nwjs

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. This is the boilerplate code, ready for the track client.
         </p>
         <a
           className="App-link"
