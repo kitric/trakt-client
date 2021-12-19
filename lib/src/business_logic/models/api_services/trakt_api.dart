583de836f39c78eac4fb6049d03e58136f5962fa
nordic16 <diogo6204@gmail.com>
2021-12-19 15:43:26 +0000

    wrote a function that will be used later for refreshing the access token.

--- lib/src/business_logic/models/api_services/trakt_api.dart
+++ lib/src/business_logic/models/api_services/trakt_api.dart
@@ -6,7 +6,6 @@ Probably will end up renaming this file later.
 
 // HTTP requests.
 import 'dart:convert';
-
 import 'package:url_launcher/url_launcher.dart';
 import 'dart:io';
 
@@ -22,12 +21,16 @@ void redirect_user(String clientID) async {
   }
 }
 
+/* 
+Retrieves access_token and refresh_token from the respective trakt api endpoint.
+*/
 Future<String> retrieveToken(
     String clientID, String clientSecret, String pin) async {
   final url =
       "https://api.trakt.tv/oauth/token?code=$pin&client_id=$clientID&client_secret=$clientSecret&redirect_uri=urn:ietf:wg:oauth:2.0:oob&grant_type=authorization_code";
 
   var client = HttpClient();
+  String access_token = "";
 
   try {
     HttpClientRequest request = await client.postUrl(Uri.parse(url));
@@ -35,10 +38,36 @@ Future<String> retrieveToken(
 
     HttpClientResponse response = await request.close();
 
-    print(json.decode((await response.transform(utf8.decoder).join())));
+    // Converts the response into a json object that can be used to retrieve access_token.
+    final r_json = json.decode((await response.transform(utf8.decoder).join()));
+    access_token = r_json['access_token'];
   } finally {
     client.close();
   }
 
-  return "";
+  // lol will refactor this later.
+  return access_token;
+}
+
+Future<String> refreshToken(String accessToken, String clientID,
+    String clientSecret, String refreshToken) async {
+  final url =
+      "https://api.trakt.tv/oauth/token?grant_type=refresh_token&client_id=$clientID&refresh_token=$refreshToken&client_secret=$clientSecret&redirect_uri=urn:ietf:wg:oauth:2.0:oob";
+
+  var client = HttpClient();
+  String newToken = "";
+
+  try {
+    HttpClientRequest request = await client.postUrl(Uri.parse(url));
+    request.headers.add('Content-Type', 'application/json');
+    HttpClientResponse response = await request.close();
+
+    // Converts the response into a json object that can be used to retrieve access_token.
+    final r_json = json.decode((await response.transform(utf8.decoder).join()));
+    newToken = r_json['access_token'];
+  } finally {
+    client.close();
+  }
+
+  return newToken;
 }
